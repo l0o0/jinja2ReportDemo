123dc952031216a9db9b9281c98a4cf17c1d8a05
l0o0 <linxzh1989@gmail.com>
2017-09-26 15:37:06 +0800

    Add templates, demo, readme and requirements.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python Jinja2 自动化报告模板
+如果需要进行测试，请先安装`requirements.txt`中的依赖。
+运行
+```
+python test.py
+```
+就可以在目录下生成`Report.html`
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
-import json
 import pprint
+from jinja2 import Environment, FileSystemLoader
 
 pp = pprint.PrettyPrinter()
 init_dict = {'dirs':{},'files':[]}
@@ -9,6 +11,8 @@ init_dict = {'dirs':{},'files':[]}
 def path_to_dict(path, init_dict):
     d = init_dict
     for x in os.listdir(path):
+        if x == '.git':
+            continue
         if os.path.isdir(os.path.join(path, x)):
             print 'dir:', x
             if x not in d['dirs']:
@@ -20,5 +24,25 @@ def path_to_dict(path, init_dict):
     
     return d
 
-mydict = path_to_dict(sys.argv[1], init_dict)
-pp.pprint(mydict)
+if __name__ == "__main__":
+    #mydict = path_to_dict(sys.argv[1], init_dict)
+    #pp.pprint(mydict)
+    temp_dir = os.path.join(os.path.abspath('.'), 'templates')
+    # 读取目录的结果
+    dir_tree = path_to_dict(os.path.abspath('.'), init_dict)
+    # 设置jinja2的环境配置
+    env = Environment(
+                    loader = FileSystemLoader(temp_dir),
+                    extensions = ['jinja2.ext.do']                                                                       )
+
+    # 读取模板文件
+    report = env.get_template('report.html')
+    # 将字典传递到模板中，进行渲染
+    with open('dirA/dirB2/Tophat-Mapping2Genome.stat.xls') as handle:
+        tophat = handle.readlines()
+        tophat = [x.strip().split('\t') for x in tophat]
+
+    html = report.render(dir_tree = dir_tree, tophat = tophat)
+
+    with open('Report.html', 'w') as handle:
+        handle.write(html.encode('utf-8'))
